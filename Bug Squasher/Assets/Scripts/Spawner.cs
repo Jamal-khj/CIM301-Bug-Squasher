8f3edc3c66c41724ed53c4e5ee512ff97ba3c74c
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2026-07-15 13:30:39 +0400

    Spawner

---
+++ Bug Squasher/Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] bugs;
+    public Vector2 spawnValues;
+    public float yPositionForSpwaner;
+    public float spawnWait;
+    public float spawnMaxTime;
+    public float spawnMinTime;
+    public int startWait;
+    public bool stop;
+
+    int randBug;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnWait = Random.Range(spawnMinTime, spawnMaxTime);
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+            randBug = Random.Range(0, bugs.Length);
+
+            Vector2 spawnPosition = new Vector2(Random.Range(-spawnValues.x, spawnValues.x), yPositionForSpwaner);
+
+            Instantiate(bugs[randBug], spawnPosition, gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
