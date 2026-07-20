fcae218b81198667078b7e690f0bfe4404bb7287
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2026-07-20 23:47:43 +0400

    Sine wave movement

---
+++ Bug Squasher/Assets/Scripts/Bugs/SineMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SineMovement : MonoBehaviour
+{
+    float sinCenterX;
+
+    // Amplitude: Distance between side to side wave
+    public float amplitude;
+    // Frequency: How often does it flatuate between the highest and lowest value of the wave
+    public float frequency;
+
+    public bool inverted = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sinCenterX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Control sine wave here
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        float sin = Mathf.Sin(pos.y * frequency) * amplitude;
+
+        if (inverted)
+        {
+            sin *= -1;
+        }
+
+        pos.x = sinCenterX + sin;
+
+        transform.position = pos;
+    }
+}
