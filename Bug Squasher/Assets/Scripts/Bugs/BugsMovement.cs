074a2885025276dea3f16af94f6596d13c9d6fba
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2026-07-20 23:47:26 +0400

    Bug movement

---
+++ Bug Squasher/Assets/Scripts/Bugs/BugsMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BugsMovement : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+
+        pos.y -= speed * Time.fixedDeltaTime;
+
+        transform.position = pos;
+    }
+}
