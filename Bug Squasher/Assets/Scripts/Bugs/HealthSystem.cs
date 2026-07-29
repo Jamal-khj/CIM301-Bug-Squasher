c37d9c7614ce232871405d28e5f2c9d9b39e769f
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2026-07-29 13:10:19 +0400

    Bugs Health Script
    - Added lines that will affect the health bar of the bugs

--- Bug Squasher/Assets/Scripts/Bugs/HealthSystem.cs
+++ Bug Squasher/Assets/Scripts/Bugs/HealthSystem.cs
@@ -5,21 +5,23 @@ public class HealthSystem : MonoBehaviour
     // Setting the max health and current health variable for the bugs
     public int maxHealth;
     public int health;
-    
+
+    public HealthBar healthBar;                 // Controls the Health Bar for the bugs
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Current health will match max health when object spawned
-        health = maxHealth;
+        health = maxHealth;                     // Current health will match max health when object spawned
+        healthBar.SetMaxHealth(maxHealth);      // Sets the Health Bar to max
     }
 
     public void TakeDamage(int damage)
     {
-        // Current health will drop when damaged. Amount depends on an external factor
-        health -= damage;
+        health -= damage;                       // Current health will drop when damaged. Amount depends on an external factor
+        healthBar.SetHealth(health);            // Sets the Health bar to reflect the current health
 
         // If current health reaches 0 or less, bugs will be destroyed
-        if(health <= 0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
