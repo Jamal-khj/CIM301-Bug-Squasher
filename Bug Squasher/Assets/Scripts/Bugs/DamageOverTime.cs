0fa4cbd4107f1f7fb07a5fa40cae844f6597513d
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2026-08-05 15:12:26 +0400

    Damage over time script

--- Bug Squasher/Assets/Scripts/Bugs/DamageOverTime.cs
+++ Bug Squasher/Assets/Scripts/Bugs/DamageOverTime.cs
@@ -6,18 +6,41 @@ public class DamageOverTime : MonoBehaviour
 {
     private PlayerHealth playerHealth;
 
-    public int dotTimer;
+    public float dotTimer;
+    public float damageTimer;
     public int dotDamage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerHealth = GetComponent<PlayerHealth>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        dotTimer += Time.deltaTime;
         
+        // Deal damage over time as long as the bug is on the player
+        if (dotTimer > damageTimer)
+            {
+                // Deal damage to the player
+                playerHealth.TakeDamage(dotDamage);
+
+                dotTimer = 0.0f;
+            }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Since bugs are prefab, we need this line to find the player component
+            if (playerHealth == null)
+            {
+                playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            }
+
+        }
     }
 }
