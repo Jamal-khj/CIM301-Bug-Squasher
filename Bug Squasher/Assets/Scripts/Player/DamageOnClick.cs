280c3db2c3e11820ab5470669ac4929fca7737da
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2026-08-05 15:13:40 +0400

    Damage On Click adjustment
    - Add new lines to damage the Heal Item object

--- Bug Squasher/Assets/Scripts/Player/DamageOnClick.cs
+++ Bug Squasher/Assets/Scripts/Player/DamageOnClick.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class DamageOnClick : MonoBehaviour
 {
     private HealthSystem healthSystem;
+    private HealPlayer healPlayer;
     public int damage = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         healthSystem = GetComponent<HealthSystem>();
+        healPlayer = GetComponent<HealPlayer>();
     }
 
     // Update is called once per frame
